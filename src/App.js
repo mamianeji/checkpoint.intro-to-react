c751f7d2b8353fd5311b17c601bba7b17c8b3cb7
mamianeji <mamia.neji@gmail.com>
2023-05-08 01:08:21 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Formulaire from './componemts/Formulaire';
+import Navbar from './componemts/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+<Navbar/>
+<Formulaire/>
+    </div>
+  ); 
+}
+
+export default App;
---
+++ src/componemts/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='nav'>
+<ul>
+<li>Home </li>
+<li>About </li>
+<li>Contact </li>
+</ul>
+
+    </div>
+    );
+}
+    
+export default Navbar
